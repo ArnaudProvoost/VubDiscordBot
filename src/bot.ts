8b608bee20093aa7566fbc1b64ad5ed30a22dd11
Arnaud Provoost <arnaud.provoost@gmail.com>
2022-12-13 15:35:35 +0100

    throw finished and aanwezig

--- src/bot.ts
+++ src/bot.ts
@@ -2,7 +2,6 @@ import { Config } from "./config";
 import { Client } from "discord.js";
 import interactionCreate from "./listeners/interactionCreate";
 import ready from "./listeners/ready";
-
 console.log("Bot is starting...");
 
 const client = new Client({
--- src/commands.ts
+++ src/commands.ts
@@ -1,5 +1,6 @@
 import { Command } from "./command";
+import { Aanwezig } from "./commands/aanwezig";
 import { Hello } from "./commands/hello";
 import { Throw } from "./commands/throw";
 
-export const Commands: Command[] = [Hello,Throw];
+export const Commands: Command[] = [Hello,Throw,Aanwezig];
