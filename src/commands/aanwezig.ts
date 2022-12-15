4ac3e88f75f529b7c586b696d9ab8fe881fc65f5
Arnaud Provoost <arnaud.provoost@gmail.com>
2022-12-15 11:19:13 +0100

    aanwezig in db_operations

--- src/commands/aanwezig.ts
+++ src/commands/aanwezig.ts
@@ -1,79 +1,30 @@
 import { Client, CommandInteraction } from "discord.js";
-import { run } from "node:test";
 import { Command } from "src/command";
-import { Database } from "../data/db";
+import { addAanwezigheidUsername, getLastAanwezigheidUsername, getUsername } from "../data/db_operations";
 
-interface User {
-    name:string;
-    totaal:number;
-    dag:number;
-}
-
-interface DB {
-    users: User[]
-}
-
-const initial: DB = {
-    users: []
-}
-
-const db = new Database<DB>('./src/data/data.json', initial)
 
 export const Aanwezig: Command = {
     name: "aanwezig",
     description: "Geef aan of je aanwezig bent.",
-    run :async (client:Client, interaction: CommandInteraction) => {
+    run: async (client: Client, interaction: CommandInteraction) => {
         var content = `<@${interaction.user.id}> is aanwezig.`
         var date = new Date()
         var day = date.getDate()
-
-        if (db.data.users[0] != undefined) {
-            var persoonfound = false;
-            var persoon
-            for (var i in db.data.users) {
-                if (db.data.users[i].name == interaction.user.username) {
-                    if (db.data.users[i].dag != day) {    
-                        db.data.users[i].totaal += 1 
-                        db.data.users[i].dag = day
-                        persoonfound = true;
-                    }
-                    else {
-                        persoonfound = true;
-                        content = `Goed geprobeerd <@${interaction.user.id}>. Je bent al aanwezig. @here Pas op <@${interaction.user.id}> probeert te cheaten.`
-                    }
-                }
-                
+        var user = getUsername(interaction.user.username)
+        if (typeof user != 'string') {
+            if (getLastAanwezigheidUsername(user.name) != day) {
+                addAanwezigheidUsername(user.name, day)
             }
-
-            if (!persoonfound) {
-                persoon = {
-                    name:interaction.user.username,
-                    totaal:1,
-                    dag: day
-                }
-                db.data.users.push(persoon)
+            else {
+                content = `Goed geprobeerd <@${interaction.user.id}>. Je bent al aanwezig. @here pas op <@${interaction.user.id}> probeert te cheaten.`
             }
-
-            db.update({
-                users:db.data.users
-            })
         }
         else {
-            db.update({
-                users: [{
-                    name:interaction.user.username,
-                    totaal:1,
-                    dag:day
-                }]
-            });
+            addAanwezigheidUsername(interaction.user.username, day)
         }
 
-        db.commit()
-
-        console.log(db.data)
-
         await interaction.followUp({
-            ephemeral:true,
+            ephemeral: true,
             content
         })
     }
---
+++ src/data/db_operations.ts
@@ -0,0 +1,92 @@
+import { Database } from "./db";
+
+interface User {
+    name: string;
+    totaal: number;
+    dag: number;
+}
+
+interface DB {
+    users: User[]
+}
+
+const initial: DB = {
+    users: []
+}
+
+const db = new Database<DB>('./src/data/data.json', initial)
+
+export function getLastAanwezigheidUsername(username: string) {
+    if (findUsername(username)) {
+        var User = getUsername(username)
+        if (typeof User == 'string') {
+            return "User not found"
+        }
+        else {
+            return User.dag
+        }
+    }
+    return "User not found"
+}
+
+function getIndexUsername (username:string) {
+
+}
+
+export function getUsername(username: string) {
+    if (findUsername(username)) {
+        for (var i in db.data.users) {
+            if (db.data.users[i].name == username) {
+                return db.data.users[i]
+            }
+        }
+        return "User not found"
+    }
+    return "User not found"
+}
+
+export function findUsername(username: string) {
+    for (var i in db.data.users) {
+        if (db.data.users[i].name == username) {
+            return true
+        }
+    }
+    return false
+}
+
+export function getUsernames() {
+    return db.data.users
+}
+
+export function addAanwezigheidUsername(username: string, dag: number) {
+    var user = getUsername(username)
+
+    if (db.data.users[0] == undefined) {
+        db.update({
+            users: [{
+                name: username,
+                totaal: 1,
+                dag: dag
+            }]
+        });
+    }
+    else {
+        if (typeof user == 'string') {
+            var persoon = {
+                name: username,
+                totaal: 1,
+                dag: dag
+            }
+            db.data.users.push(persoon)
+        }
+        else {
+            user.totaal += 1
+            user.dag = dag
+        }
+        db.update({
+            users: db.data.users
+        })
+    }
+
+    db.commit()
+}
